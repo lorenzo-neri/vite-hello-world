d9c8ccb48f82311c484a9309ebdbc47aa720377e
lorenzo-neri <lorenzo21neri@gmail.com>
2023-09-21 16:41:29 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello Vite!'
+
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
